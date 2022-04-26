60895887f3d77a633ae78c26b846997d1f2d54f6
Deepanshu <97582830+DeepanshuDaksh77@users.noreply.github.com>
2022-04-26 15:25:47 +0530

    Add files via upload
    today's lecture on strings

---
+++ strings.py
@@ -0,0 +1,40 @@
+#string handling and manupulation
+d ='aman\n raman\n shaman\t\thest'
+print (d)
+#triple quotes are usedd to 
+r ="""aman raman, 
+shaman ,
+ghhjh hkhg gjhf fhgf gjj hest"""
+print (r)
+#string concatenation
+z = "asdsadsad" 
+x = "asdfasdfasdf"
+print(z+x)
+#repetition
+print(z*100)
+#indexing
+print(z[1])
+#slicing
+print(z[1:4])
+#slicing by giving upper and lower limits
+f='gurdaspur'
+print(f[:7:2])
+#for length
+print(len(f))
+#to reverse the string
+print(f[::-1])
+#string type conversion
+sd='42'
+sr= str(1)
+ft = sd + sr
+print(ft)
+#string capitalize
+r='aman is a good boy'
+print(r.capitalize())
+#returns number of time a word has been repeated
+aa = "number 1 number 2 number3"
+xx = aa.count("number")
+print(xx)
+#converts to lowercase
+c = "HOHOHOHOHOHOHO"
+print(c.casefold())
